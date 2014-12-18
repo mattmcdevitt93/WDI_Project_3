f34855e2519e981515554d829c2389039a5941f7
mattmcdevitt93 <mattmcdevitt93@gmail.com>
2014-12-18 14:21:34 -0500

    image fix for GH-pages for real this time... again

--- app/scripts/cookie.js
+++ app/scripts/cookie.js
@@ -32,11 +32,12 @@ Cookie.getCookie = function(key){
 };
 
 Cookie.gameToken = function(game_id) {
+  debugger;
+  console.log('generate ID cookie' + game_id);
   if ((Cookie.getCookie('Game_id') === 'null') || (Cookie.getCookie('Game_id') === undefined) || (Cookie.getCookie('Game_id') === null)) {
-    console.log('generate ID cookie');
-    var game_token = 'Game_ID=' + game_id;
-    debugger;
-    document.cookie = game_token;
+    // var game_token = 'Game_ID=' + game_id;
+    // document.cookie = game_token;
+    Cookie.makeCookie('Game_ID', game_id);
   } else {
     console.log('ID already set at = ' + Cookie.getCookie('User'));
   }
@@ -44,4 +45,9 @@ Cookie.gameToken = function(game_id) {
 
 Cookie.deleteCookie = function(name) {
     document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
+
+Cookie.makeCookie = function(key, token) {
+  var temp = key + '=' + token;
+  document.cookie = temp;
 };
